ff289ffaf6d946ccdd925c1c03f6ea17c5bdcf6d
Léo Viggiano <leoviggiano1@outlook.com>
2019-10-16 18:58:27 -0300

    add: user update

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -17,7 +17,33 @@ class UserController {
   }
 
   async update(req, res) {
-    return res.json({ ok: true });
+    const { email, password, oldPassword } = req.body;
+    const user = await User.findByPk(req.userId);
+
+    if (email && email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+      if (userExists)
+        return res.status(400).json({ error: 'User already exists' });
+    }
+
+    if (password && !oldPassword) {
+      return res
+        .status(400)
+        .json({ error: 'You must inform your old password' });
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(400).json({ error: 'Invalid password' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 
